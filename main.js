acd28a2a849471ecb0fb75ad42454640cf2f964b
laura <106088132+lalinha07@users.noreply.github.com>
2024-03-12 10:25:18 -0300

    g

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const botoes = document.querySelectorAll( ".botao")
 for (lef i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for (lef j = 0; i < botoes.length; j++){
-            botoes[j].classList.remove("ativo")
+            botoes[j].classlist.remove("ativo")
         }
-        botoes[i].classList.add("ativo")
+        botoes[i].classlist.add("ativo")
     }
 }
