36738527aac04ea428828d26fcc2d6b72e22079f
laura <106088132+lalinha07@users.noreply.github.com>
2024-04-30 10:25:58 -0300

    k

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const botoes = document.querySelectorAll(".botão")
-const textos = document.querySelectorAll(".aba-conteudo")
+const botoes = document.querySelectorAll(".botão");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function (){
@@ -7,19 +7,19 @@ for (let i = 0; i < botoes.length; i++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2024-12-01T00:00:00")
+const tempoObjetivo1 = new Date ("2024-12-01T00:00:00");
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
     contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000)
-    let minutos = Math.floor(segundos / 60)
-    let horas = Math.floor(minutos / 60)
-    let dias = Math.floor(horas / 24)
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
       
         segundos %= 60;
         minutos %= 60;
